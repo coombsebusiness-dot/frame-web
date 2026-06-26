99429bcb19eee7e807d87867b8b2a7f9f8e8a527
coombsebusiness-dot <coombs.e.business@gmail.com>
2026-06-26 18:12:56 +0100

    Add Google Ads tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +30,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><Script
+  src="https://www.googletagmanager.com/gtag/js?id=AW-18275620443"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-ads" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-18275620443');
+  `}
+</Script>{children}</body>
     </html>
   );
 }
